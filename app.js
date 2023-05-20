6e5e74a7390158d0280a1ecbe37f93ef083d2c86
Reillat Anthony <anthony.reillat@epita.fr>
2023-05-20 18:09:51 +0200

    express-quests

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-const express = require("express");
 require("dotenv").config();
+const express = require("express");
+
 
 const app = express();
+
 app.use(express.json());
 
+// const port = 5000;
 const port = process.env.APP_PORT ?? 5000;
+
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
@@ -16,10 +20,17 @@ const movieHandlers = require("./movieHandlers");
 const userHandlers = require("./userHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
+app.post("/api/movies", movieHandlers.postMovie);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 
 app.get("/api/users", userHandlers.getUsers);
+app.post("/api/users", userHandlers.postUser);
 app.get("/api/users/:id", userHandlers.getUsersById);
+app.put("/api/users/:id", userHandlers.updateUser);
+app.delete("/api/users/:id", userHandlers.deleteUser);
+
 
 app.listen(port, (err) => {
   if (err) {
@@ -28,5 +39,3 @@ app.listen(port, (err) => {
     console.log(`Server is listening on ${port}`);
   }
 });
-
-
